b033dc4f68991235ddcce19e19991e2c97cb3cc3
harishkumarks514 <44613619+harishkumarks514@users.noreply.github.com>
2022-09-19 19:28:12 +0530

    Add files via upload

--- librarymanagementsystem/src/test/java/com/pack/librarymanagementsystem/LibrarymanagementsystemApplicationTests.java
+++ librarymanagementsystem/src/test/java/com/pack/librarymanagementsystem/LibrarymanagementsystemApplicationTests.java
@@ -13,21 +13,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.mock.web.reactive.function.server.MockServerRequest.Builder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.pack.librarymanagementsystem.entity.Book;
 import com.pack.librarymanagementsystem.repository.BookRepository;
 import com.pack.librarymanagementsystem.service.BookService;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.OngoingStubbing;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -35,7 +29,6 @@ class LibrarymanagementsystemApplicationTests {
 
 	@Autowired
 	private BookService bookService;
- 
 
 	@MockBean
 	private BookRepository bookRepository;
@@ -47,7 +40,7 @@ class LibrarymanagementsystemApplicationTests {
 		Mockito.when(bookRepository.save(book)).thenReturn(book);
 		assertEquals(book, bookService.saveBook(book));
 	}
-	
+
 //	@ParameterizedTest
 //	public void saveAllBookTest() {
 //		Book book = new Book(01, "Half Girlfriend", "Chethan bhaghath", "GK", true, "");
@@ -59,28 +52,31 @@ class LibrarymanagementsystemApplicationTests {
 //		Mockito.when(bookService.saveBulkBook(books));
 //		assertEquals(book, books.get(0));
 //	}
-	
+
 	@Test
 	public void getBookForUserTest() {
 		Book book = new Book(01, "Half Girlfriend", "Chethan bhaghath", "GK", true, "");
-		Mockito.when(bookService.getBookForUser("Chethan bhaghath")).thenReturn((Book) Stream.of(book));
-		assertEquals(book,bookService.getBookForUser("Chethan bhaghath"));
-	} 
+		Mockito.when(bookService.getBookForUser("Chethan bhaghath")).thenReturn((book));
+		assertEquals(book, bookService.getBookForUser("Chethan bhaghath"));
+	}
 
-	@ParameterizedTest
+	@Test
 	public void updateBook() {
 		Book book = new Book(04, "Half Girlfriend", "Chethan bhaghath", "GK", true, "");
-		Mockito.when(bookService.updateBookDetails(book))
-				.thenReturn(book);
+		Mockito.when(bookService.updateBookDetails(book)).thenReturn(book);
 		assertEquals(book, bookService.updateBookDetails(book));
 	}
 
-	 
-
 	@ParameterizedTest
 	public void getBookByTopicTest(String author, Boolean isIssued) {
+		
 		Book book = new Book(01, "Half Girlfriend", "Chethan bhaghath", "GK", true, "");
-		Mockito.when(bookService.searchBooksByTopic("GK")).thenReturn((List<Book>) Stream.of(book));
+		List<Book> books = new ArrayList();
+		books.add(book);
+		books.add(1, book);
+//		Book book = new Book(01, "Half Girlfriend", "Chethan bhaghath", "GK", true, "");
+		Mockito.when(bookService.searchBooksByTopic("GK")).thenReturn(books);
+		assertEquals(books, books);
 		assertEquals(book, bookService.searchBooksByTopic("GK"));
 	}
 
